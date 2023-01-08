c16353c7cc98853192d521cd859368fbf650b767
smukhyala <smukhyala@gmail.com>
2023-01-08 13:58:53 -0800

    fixing the no email issue

--- src/hello/app.py
+++ src/hello/app.py
@@ -409,10 +409,12 @@ class RepeatTimer(Timer):
 ### Toga running main function + setup
 def main():
     #return toga.App('Hello', 'org.SanjayMukhyala.PerfectDay', startup = build)
-    d = Daemon()
-    timer = RepeatTimer(10,d.job)  
-    timer.start()
+    
     return DemoApp()
+
+d = Daemon()
+timer = RepeatTimer(10,d.job)  
+timer.start()
 
 #time.sleep() #instances
 #timer.cancel()
